5449b8cc450d67d51258481a81f7fe231935adc0
Ricardo <ricardoivalle@hotmail.com>
2019-11-28 16:20:11 -0300

    Corrections CR

--- N8Tests/Program.cs
+++ N8Tests/Program.cs
@@ -17,7 +17,7 @@ namespace N8Tests
             Console.WriteLine(calculatedValue);
         
         }
-
+        
         private void CalculateOp(int operationId,int firstVal, int secondVal)
         {
             switch(operationId)
@@ -32,6 +32,7 @@ namespace N8Tests
                 return firstVal * secondVal;
 
                 case (int)Operations.DIVIDE:
+                if (secondVal == 0){return null;}
                 return (firstVal / secondVal);
 
             }
