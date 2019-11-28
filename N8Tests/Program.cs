e44f348204ee47394d0299878a9814bf94f34f27
Ricardo <ricardoivalle@hotmail.com>
2019-11-28 15:58:09 -0300

    Added default value

--- N8Tests/Program.cs
+++ N8Tests/Program.cs
@@ -4,9 +4,11 @@ namespace N8Tests
 {
     class Program
     {
+        private const int DEFAULT_VALUE = 32;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int defaultValue = DEFAULT_VALUE;
+            Console.WriteLine("The default value is: " + defaultValue.ToString());
         }
     }
 }
